25773d3583ccf0efdb2bd89f18792c488f4f282b
Tigran Vartanyan <tvartanyan29@yandex.ru>
2024-08-11 13:25:58 +0400

    Add a little JS

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const $circle = document.querySelector('#circle');
+const $score = document.querySelector('#score');
+
+$circle.addEventListener('click', (event) => {
+  const rect = $circle.getBoundingClientRect() 
+
+  const offfsetX = event.clientX - rect.left - rect.width / 2
+  const offfsetY = event.clientY - rect.top - rect.height / 2
+
+  const DEG = 40
+
+  const tiltX = (offfsetY / rect.height) * DEG
+  const tiltY = (offfsetX / rect.width) * -DEG
+
+  $circle.style.setProperty('--tiltX', `${tiltX}deg`)
+  $circle.style.setProperty('--tiltY', `${tiltY}deg`)
+
+  setTimeout(() => {
+    $circle.style.setProperty('--tiltX', `0deg`)
+    $circle.style.setProperty('--tiltY', `0deg`)
+  }, 300)
+})
+
+/* 18:44 */
